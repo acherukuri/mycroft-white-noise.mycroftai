4b0ea1ff3422fc1b2086e1107f820f89bf692338
Anil Cherukuri <anil.cherukuri03@gmail.com>
2019-04-06 23:30:30 -0500

    intent file handler plays & turns on just white noice

--- __init__.py
+++ __init__.py
@@ -15,42 +15,52 @@
 # You should have received a copy of the GNU General Public License
 # along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
 import time
+from adapt.intent import IntentBuilder
 from os.path import dirname, join
+from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
-from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
+from mycroft import intent_file_handler
+from mycroft.skills.audioservice import AudioService
 from mycroft.util.parse import match_one, extract_duration
 
 LOGGER = getLogger(__name__)
 
-track_list = {
-    'white noise for': join(dirname(__file__), "whitenoise.mp3"),
-    'white noise waves for': join(dirname(__file__), "waves.mp3"),
-    'white noise rain for': join(dirname(__file__), "rain.mp3"),
-    'white noise wind for': join(dirname(__file__), "wind.mp3"),
-}
+class WhiteNoiseSkill(MycroftSkill):
+    def __init__(self):
+        super(WhiteNoiseSkill, self).__init__(name="WhiteNoiseSkill")
+        self.play_list = {
+            0: join(dirname(__file__), "whitenoise.mp3"),
+            'white noise waves for': join(dirname(__file__), "waves.mp3"),
+            'white noise rain for': join(dirname(__file__), "rain.mp3"),
+            'white noise wind for': join(dirname(__file__), "wind.mp3"),
+        }
 
-class WhiteNoiseSkill(CommonPlaySkill):
-    def CPS_match_query_phrase(self, phrase):
-        match, confidence = match_one(phrase, track_list)
-        if confidence > 0.5:
-            return (match, CPSMatchLevel.TITLE, {"track": match})
+    def initialize(self):
+        self.audio_service = AudioService(self.bus)
+
+    def handle_white_noise_intent(self, message):
+        utterance = message.data.get('utterance', "")
+        try:
+            track_duration = int(extract_duration(utterance)[0].total_seconds())
+        except AttributeError:
+            return None
+        if(track_duration > 0):
+            self.log.info('track_duration is: ' + str(track_duration))
+            self.audio_service.play(join(dirname(__file__), "whitenoise.mp3"),
+                                    utterance, True)
+            while track_duration > 0:
+                time.sleep(1)
+                track_duration -=1
+            self.audio_service.stop()
         else:
             return None
 
-    def CPS_start(self, phrase, data):
-        url = data['track']
-        try:
-            track_duration = int(extract_duration(phrase)[0].total_seconds())
-        except AttributeError:
-            return None
-        self.log.info('track_duration is: ' + str(track_duration))
-        self.log.info('track name is: ' + url)
-        self.audioservice.play(url, phrase, True)
-        while track_duration > 0:
-            time.sleep(1)
-            track_duration -=1
-        self.audioservice.stop()
+    @intent_file_handler("play.white.noice.intent")
+    def handle_query_time_alt(self, message):
+        self.handle_white_noise_intent(message)
 
+    def stop(self):
+        pass
 
 def create_skill():
     return WhiteNoiseSkill()
